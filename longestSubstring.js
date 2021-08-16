a64a9128cdd52fde0562e4c4d5c2fba24048f69d
xpert-uv <72463141+xpert-uv@users.noreply.github.com>
2021-08-16 13:56:51 -0400

    final commit

---
+++ longestSubstring.js
@@ -0,0 +1,46 @@
+/** 
+ * Function (longestSubstring) takes array of words
+ * return  { letter:"",
+ *            length:   }
+ * 
+ * Constrains:
+ * Each word consists of lowercase letters (a-z).
+ * no null / udefined character.
+ */
+
+/** Step1: concat the words form array.
+ * Step2: count the frequency of char from words
+ * step3: return the object with higest frequency.
+ * 
+ */
+const storeCount = {};
+const longestSubstring = arryWords => {
+   
+    arryWords.map(words => {
+       
+        [...words].map(char => {
+            console.log(char);
+            if (storeCount[char]) {
+               storeCount[char]+=  1
+            } else {
+                storeCount[char] = 1;
+            }                 
+            })
+    })
+    
+    // returns the highest frequency 
+    const value = Math.max(...Object.values(storeCount));
+   
+    // loops over the object finds the key for highest frequency 
+    for (const key in storeCount) {
+        if (storeCount[key] === value) return {
+            "letter": key,
+            "length": value
+        } 
+    }
+
+
+    }
+
+    
+// this has O(n) complexity 
